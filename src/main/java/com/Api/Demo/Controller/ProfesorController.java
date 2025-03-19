be4c5343023419ef7066be1b4b4772beeeffed4d
JemerUCR <jemerson0095@gmail.com>
2025-03-18 23:06:49 -0600

    Ticket03

--- src/main/java/com/Api/Demo/Controller/ProfesorController.java
+++ src/main/java/com/Api/Demo/Controller/ProfesorController.java
@@ -1,5 +1,15 @@
 package com.Api.Demo.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
 public class ProfesorController {
+    @GetMapping("/")
+    public String listar(@RequestParam String param) {
+        return new String();
+    }
     
+
 }
