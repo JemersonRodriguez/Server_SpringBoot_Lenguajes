3d067a901f90a1fb41597f709bd1e508ae15ac5a
JemerUCR <jemerson0095@gmail.com>
2025-03-18 18:47:37 -0600

    EndPoints Object Json Returned

--- src/main/java/com/Api/Demo/Controller/UserController.java
+++ src/main/java/com/Api/Demo/Controller/UserController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+import com.Api.Demo.DTO.UserDTO;;
 
 
 @RestController
 public class UserController {
     @GetMapping("/")
-    public String getMethodName() {
-        return "Hola desde el Server desde SpringBoot";
+    public UserDTO getMethodName() {
+        UserDTO user = new UserDTO("Jemerson", "Informatica Empresarial", 26);
+        return user;
     }
-    
 }
