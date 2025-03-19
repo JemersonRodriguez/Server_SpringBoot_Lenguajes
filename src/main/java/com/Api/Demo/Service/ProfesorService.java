0fb7e61a190fac00f4b414799196fe0a38e91fbe
JemerUCR <jemerson0095@gmail.com>
2025-03-18 22:51:53 -0600

    New ChangesX2

--- src/main/java/com/Api/Demo/Service/ProfesorService.java
+++ src/main/java/com/Api/Demo/Service/ProfesorService.java
@@ -7,17 +7,39 @@ import com.Api.Demo.Model.Profesor;
 public class ProfesorService {
     private ArrayList<Profesor> listaProfesores;
 
-    public ProfesorService () {
+    public ProfesorService() {
         this.listaProfesores = new ArrayList<Profesor>();
     }
 
-    public ArrayList<Profesor> getListaProfesores () {
+    public ArrayList<Profesor> listar() {
         return listaProfesores;
     }
 
-    public void agregar () {}
-    public void listar () {}
-    public void consultar () {}
-    public void actualizar () {}
-    public void eliminar () {}
+    public boolean agregar(Profesor profesor) {
+        listaProfesores.add(profesor);
+        return true;
+    }
+
+    public String consultar(Profesor profesor) {
+        for (int i = 0; i < listaProfesores.size(); i++) {
+            if (listaProfesores.get(i).equals(profesor)) {
+                return listaProfesores.get(i).toString();
+            }
+        }
+        return null;
+    }
+
+    public boolean actualizar(Profesor profesor) {
+        return false;
+    }
+
+    public boolean eliminar(Profesor profesor) {
+        for (int i = 0; i < listaProfesores.size(); i++) {
+            if (listaProfesores.get(i).equals(profesor)) {
+                listaProfesores.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
